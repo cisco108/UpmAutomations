e36313307f909853d0ce5b726f543c93385a6948
linus@windows <linus@windows.de>
2025-04-12 21:48:33 +0200

    Init with basic setup.

---
+++ Editor/GitBashCommandBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class GitBashCommandBuilder 
+{
+
+    public string GetCommit(string contentPath)
+    {
+        //TODO: this has to have the version in it 
+        return GitCommands.add + contentPath
+                               + " && " + GitCommands.commit_m
+                               + $" \' added {contentPath} on {DateTime.Now} \'";
+    }
+
+    public string GetPush(string branchName = default)
+    {
+        if (branchName == default)
+        {
+            return GitCommands.push_origin;
+        }
+        return GitCommands.push_origin + branchName;
+    }
+
+    public string GetPushAllBranches()
+    {
+        return GitCommands.push_origin + GitFlags.all;
+    }
+
+    public string GetCreateBranch(string branchName)
+    {
+        return GitCommands.branch + branchName;
+    }
+
+    public string GetCurrentBranch()
+    {
+        return GitCommands.branch + GitFlags.show_current;
+    }
+
+    public string GetSwitch(string branch)
+    {
+        return GitCommands.g_switch + branch;
+    }
+
+    public string GetSubtreeSplitNewBranch(string prefix, string newBranchName)
+    {
+        return GitCommands.subtree_split_prefix + prefix + GitFlags.branch + newBranchName;
+    }
+
+}
---
+++ Editor/UpmAutoMain.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class UpmAutoMain
+{
+    private static GitBashInterface _terminal;
+    private static GitBashCommandBuilder _commandBuilder;
+
+    static UpmAutoMain()
+    {
+        _terminal = new GitBashInterface();
+        _commandBuilder = new GitBashCommandBuilder();
+        UpmAutoGui.OnPublish += Main;
+    }
+
+  
+    private static void Main()
+    {
+        Debug.Log("Main");
+    }
+
+}
+
+public static class Configs
+{
+    public static string gitBashExe = @"C:\Program Files\Git\git-bash.exe";
+    public static string branchName;
+    public static string version;
+    public static string packagePath = @"Assets\";
+}
